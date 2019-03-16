1408a906546255c1833665b0d5c0472f5ed8e5e9
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-16 15:45:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
   array_element_squared = 0
-  array.each do |array_element|
-    new_array << Integer.sqrt(array_element)
+  array.each do |array|
+    new_array << Integer.sqrt(array)
     array_element_squared += 1
   end
   new_array
